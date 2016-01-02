146dbc0f9f2152cad6ae9c9804dc16ab6da3e6b3
Tim Nguyen <ntim.bugs@gmail.com>
2016-01-02 22:13:12 +0100

    Simplify lock shape

---
+++ viewer/js/icon-list.js
@@ -0,0 +1,33 @@
+var IconList = {
+  getAllDirectories() {
+    return new Promise(function(resolve, reject) {
+      request({
+        url: "http://api.github.com/repos/nt1m/firefox-svg-icons/commits"
+      }).then(function(commits) {
+        request({
+          url: "http://api.github.com/repos/nt1m/firefox-svg-icons/git/trees/" + commits[0].sha + "/icons/"
+        }).then(function(response) {
+          resolve(response);
+        });
+      });
+    });
+  }
+};
+function request(options) {
+  var method = options.method || "GET";
+
+  var req = new XMLHttpRequest();
+  req.responseType = "json";
+  req.open(method, options.url);
+
+  var promise = new Promise(function (resolve, reject) {
+    req.addEventListener("load", () => {
+      resolve(req.response);
+    });
+    req.addEventListener("error", reject);
+  });
+
+  req.send(JSON.stringify(options.data));
+
+  return promise;
+};
